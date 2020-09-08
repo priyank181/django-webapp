c1320dffea2bca35e2529bee38b04c8421e762c1
priyank181 <43669347+priyank181@users.noreply.github.com>
2020-09-09 00:20:08 +0530

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,12 +25,14 @@ SECRET_KEY = '@-y-h1963n7-c*&je*)ixix&u9td4wev85!c_zq^5+2kvtt4u5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["priyank181.herokuapp.com"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -66,7 +68,34 @@ TEMPLATES = [
         },
     },
 ]
-
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
