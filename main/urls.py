703c8309276238531c43808e81de49a124676b1d
priyank181 <43669347+priyank181@users.noreply.github.com>
2020-09-09 16:17:51 +0530

    make it better

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-    path("", views.login_request, name="homepage"),
+    path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
     path("logout", views.logout_request, name="logout"),
     path("login", views.login_request, name="login"),
